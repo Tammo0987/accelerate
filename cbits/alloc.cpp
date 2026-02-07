c630792773e70746192fd2a92e0d152eb9f21891
Daniel van Opstal <dvopstal05@gmail.com>
2026-02-07 15:56:17 +0100

    use _aligned_malloc on windows instead of parlaylib

--- cbits/alloc.cpp
+++ cbits/alloc.cpp
@@ -1,10 +1,26 @@
+#ifdef _WIN32
+#include <cstdlib>
+#include <cstdint>
+#else
 #include "./parlaylib/include/parlay/alloc.h"
+#endif
 
 extern "C" {
+
   void* accelerate_raw_alloc(uint64_t size, uint64_t align) {
+#ifdef _WIN32
+    return _aligned_malloc(size, align);
+#else
     return parlay::p_malloc(size, align);
+#endif
   }
+
   void accelerate_raw_free(void *ptr) {
+#ifdef _WIN32
+    _aligned_free(ptr);
+#else
     parlay::p_free(ptr);
+#endif
   }
+
 }
