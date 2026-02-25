aa64775d1a26877733b9c5c19417bf40bed24195
Daniel van Opstal <dvopstal05@gmail.com>
2026-02-25 11:23:18 +0100

    move select optimization from Simplify to Bounds

--- src/Data/Array/Accelerate/Trafo/Exp/Simplify.hs
+++ src/Data/Array/Accelerate/Trafo/Exp/Simplify.hs
@@ -311,14 +311,7 @@ simplifyOpenExp env = first getAny . cvtE
            -> (Any, PreOpenExp arr env t)
            -> (Any, PreOpenExp arr env t)
            -> (Any, PreOpenExp arr env t)
-    select p t@(_,t') e@(_,e')
-      | Just Refl <- matchTypeR (expType t') (TupRsingle scalarTypeWord8)
-      = case (t', e') of
-        (_        , Const _ 0)  -> yes $ PrimApp PrimLAnd (Pair p t')                    -- p ? t : 0 => p && t
-        (Const _ 0, _        )  -> yes $ PrimApp PrimLAnd (Pair (PrimApp PrimLNot p) e') -- p ? 0 : e => (not p) && e
-        (Const _ 1, _        )  -> yes $ PrimApp PrimLOr  (Pair p e')                    -- p ? 1 : e => p || e
-        (_        , Const _ 1)  -> yes $ PrimApp PrimLOr  (Pair (PrimApp PrimLNot p) t') -- p ? t : 1 => (not p) || t
-        _                       -> Select p <$> t <*> e
+    select p t e
       | PrimApp PrimLNot c <- p = yes $ snd $ select c e t
       | otherwise               =  Select p <$> t <*> e
 
