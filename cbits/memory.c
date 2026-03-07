775507616eb59fc688013332e5a497a6b7a16f41
Daniel van Opstal <dvopstal05@gmail.com>
2026-03-07 13:08:12 +0100

    track memory allocations

--- cbits/memory.c
+++ cbits/memory.c
@@ -5,6 +5,7 @@
 #include <stdatomic.h>
 #ifdef ACCELERATE_MEMORY_COUNTER
 #include <pthread.h>
+#include "tracy/public/tracy/TracyC.h"
 #endif  // ACCELERATE_MEMORY_COUNTER
 
 #include "align.h"
@@ -67,8 +68,8 @@ void* accelerate_buffer_alloc(uint64_t byte_size) {
   size = (size + CACHE_LINE_SIZE - 1) / CACHE_LINE_SIZE * CACHE_LINE_SIZE;
   void *ptr = accelerate_raw_alloc(size, CACHE_LINE_SIZE);
 
-  // TODO: call ___tracy_emit_memory_alloc
   #ifdef ACCELERATE_MEMORY_COUNTER
+  ___tracy_emit_memory_alloc(ptr, size, 0);
   // For benchmarking memory usage:
   pthread_mutex_lock(&mutex);
   total_allocated += byte_size;
@@ -107,9 +108,8 @@ void accelerate_buffer_release_by(void* interior, uint64_t amount) {
   // Release is always needed, acquire only when the reference count drops to zero.
   uint64_t old = atomic_fetch_add_explicit(&header->reference_count, -amount, memory_order_acq_rel);
   if (old == amount) {
-    // TODO: call ___tracy_emit_memory_free
-
     #ifdef ACCELERATE_MEMORY_COUNTER
+    ___tracy_emit_memory_free(header, 0);
     pthread_mutex_lock(&mutex);
     current_allocated -= header->byte_size;
     pthread_mutex_unlock(&mutex);
